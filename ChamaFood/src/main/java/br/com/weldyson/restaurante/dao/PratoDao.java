00dd76dcce649b920759c74739e64b834b84d04e
Azevedo <weldysona@gmail.com>
2025-06-26 21:36:48 -0300

    Adicionado o CRUD na classe DAO

--- ChamaFood/src/main/java/br/com/weldyson/restaurante/dao/PratoDao.java
+++ ChamaFood/src/main/java/br/com/weldyson/restaurante/dao/PratoDao.java
@@ -8,19 +8,37 @@ public class PratoDao {
 
     private EntityManager entityManager;
 
-    public PratoDao(EntityManager entityManager){
+    public PratoDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
+         /*
+        CRUD
+        C = CREAT
+        R= READ
+        U= UPDATE
+        D= DELETE
+         */
 
-    public void cadastrar (Prato prato){
+
+    public void cadastrar(final Prato prato) {
         this.entityManager.persist(prato);
 
         System.out.println("Entidade cadastrada: " + prato);
+
+
     }
 
+    public Prato consultar(final Integer id) {
+        return this.entityManager.find(Prato.class, id);
 
+    }
 
-
+    public void atualizar(final  Prato prato){
+        this.entityManager.merge(prato);
+    }
+    public void excluir(final Prato prato){
+        this.entityManager.remove(prato);
+    }
 
 }
--- ChamaFood/src/main/java/br/com/weldyson/restaurante/service/teste/PratoService.java
+++ ChamaFood/src/main/java/br/com/weldyson/restaurante/service/teste/PratoService.java
@@ -24,6 +24,7 @@ public class PratoService {
         entityManager.getTransaction().commit();
         entityManager.close();
 
+
     }
 
 }
